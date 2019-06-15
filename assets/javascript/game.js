3f03b2c5fe6b0fc79726e2fed48d96ad27717052
hunting <mafalda.villazan@gmail.com>
2019-06-15 16:44:07 -0700

    Adding the Key code to the section of 'letters used'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,56 @@
-//Create the array of desingers
+//Load when the document is ready
 
-var desingers = [
-    "Saul Bass",
-    "Stefan Sagmeister",
-    "Zaha Hadid",
-    "Paul Rand"
-]
+$(document).ready(function () {
 
-// Create a function event for on when the key is up or after pressing
-document.onkeyup = function () {
+    //Create the array of desingers
 
-    //Create a a variable that selects a random designer
-    var computerSelection = desingers[Math.floor(Math.random() * desingers.length)];
-    //Write what computer selected on the screen
-    $("#wordGuess").append(computerSelection);
+    var desingers = [
+        "Saul Bass",
+        "Stefan Sagmeister",
+        "Zaha Hadid",
+        "Paul Rand"
+    ]
 
-};
+    //Create a function event for on when the key is up or after pressing
+    $("#start").click(function (event) {
+
+        //Create a variable that selects a random number inside our array
+        var arrNum = Math.floor(Math.random() * desingers.length)
+        //Relate that number with a person inside our array
+        var computerSelection = desingers[arrNum];
+        //Write what computer selected on the screen
+        $("#wordGuess").append(computerSelection);
+        //Add _ multiply by the number of letters
+
+
+        //Visualizing the letters inside the string selected
+        // Found charAt in w3 school "The charAt() method returns the character at the specified index in a string.""
+        for (var i = 0; i < desingers[arrNum].length; i++) {
+            var letterArray = desingers[arrNum].charAt(i);
+        }
+
+    });
+
+
+
+    //Showcase the letter the person has selected
+    // Adding the text on the screen
+
+    //Create a varible that has the value of the key pressed
+
+    $(document).keyup (function (event) {
+        $("#letterUsed").append(" " + event.keyCode);
+    });
+
+
+
+    //Function that compares your letter with the letters of the array selected
+    //if (letterPerson === letterArray) {}
+
+
+
+
+
+});
+
 
