c8d402fa9b7a445bec4f3778cc2989aca592ae63
hunting <mafalda.villazan@gmail.com>
2019-06-16 13:43:42 -0700

    Adding if and else statement to compare it with if the value has it or not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,27 +25,76 @@ $(document).ready(function () {
 
         //Visualizing the letters inside the string selected
         // Found charAt in w3 school "The charAt() method returns the character at the specified index in a string.""
-        for (var i = 0; i < desingers[arrNum].length; i++) {
-            var letterArray = desingers[arrNum].charAt(i);
+
+        //Learned from meetup
+        // Keeps track of current guesses
+        const currentGuesses = new Set([]);
+
+
+
+
+        function checkingLetter(selection) {
+            for (var i = 0; i < desingers[arrNum].length; i++) {
+
+                //Ask? why do we need to add string if it was a string before.
+                var letterArray = String(desingers[arrNum]).charAt(i);
+
+                //creating an if that runs through all of the letters of the selected index and compares it with the ltter selected
+                if (selection === letterArray.toLowerCase()) {
+                    // Adding the lettere selected to currentGuesses
+                    currentGuesses.add(selection);
+                    $ ("#wordGuessAdd").append(selection);
+                
+
+                }
+                else {
+                    
+                    console.log("wrong");
+                }
+            }
         }
 
+
+
+
+
+        //Showcase the letter the person has selected
+        //Create a varible that has the value of the key pressed
+        // Adding the text on the screen
+        $(document).keyup(function (event) {
+            var letterSelected = event.key;
+            console.log(event.key);
+            $("#letterUsed").append(letterSelected + ", ");
+
+            checkingLetter(letterSelected);
+
+            //Checking the return
+            if (checkingLetter()) {
+                //alert("letter is correct");
+            }
+            else {
+                //alert("letter NO correct")
+            };
+
+
+        });
+
+
+
+        // Stop adding letters if they have already been typed
+        //if (letterSelected == ("#letterUsed")) {
+        //   alert("You already selected this Letter")
+        //};
+
+        //Compare the letters from a sting to the letter selected
+
+
+
+
     });
 
 
 
-    //Showcase the letter the person has selected
-    // Adding the text on the screen
-
-    //Create a varible that has the value of the key pressed
-
-    $(document).keyup (function (event) {
-        $("#letterUsed").append(" " + event.keyCode);
-    });
-
-
-
-    //Function that compares your letter with the letters of the array selected
-    //if (letterPerson === letterArray) {}
 
 
 
