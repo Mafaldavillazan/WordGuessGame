471a2c9d512f41300840aebfe81bfc800661ce87
hunting <mafalda.villazan@gmail.com>
2019-06-20 20:15:27 -0700

    managing to add a letter to the text when guessing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,6 +20,10 @@ $(document).ready(function () {
     var secretPoints = 0;
     $("#wins").text(wins);
 
+    //Create an empty array that we will fill with ---
+    var answerArr = [];
+
+
 
     //Create a function that will add points if the word is completed ( maybe )
 
@@ -43,39 +47,61 @@ $(document).ready(function () {
         var computerSelection = desingers[arrNum];
 
 
-        //Create a function theat showcase the - for each letter of the name selected
-        function lineDisplay(randomSelection) {
 
-            for (var i = 0; i < randomSelection.length; i++) {
-                //Search for the space between my two words (Name & Surname)
-                if (randomSelection[i] === " ") {
-                    // Add space into HTML (search the solution on google with meetup team)
-                    $("#wordGuess").append("&nbsp;");
-                    // when they win I add a point to the secret selection
-                    secretPoints++
-                }
-                else {
-                    $("#wordGuess").append(" _ ");
-                }
+        //Array with ---
+        var computerSelectionArrLines = [];
+        //Array with the index of the number
+        var computerSelectionArrIndex = [];
 
-                //I compare the lenght of the word selected with the numer of points the aquire whrn they win
-                if (computerSelection.length === secretPoints) {
-                    alert("YOU WON!")
-                    wins++
+        //create a for loop that will go through all the letters and change them to - and the space to ""
+        for (var i = 0; i < computerSelection.length; i++) {
 
-                }
-                else {
-                    var guessCompleted = randomSelection.length - secretPoints
-                    console.log("Guesses " + guessCompleted);
-
-                };
+            if (computerSelection[i] === " ") {
+                // Add space into HTML (search the solution on google with meetup team)
+                answerArr[i] = "&nbsp;";
+            }
+            else {
+                answerArr[i] = "-";
+                //I am pushing the  index of the letter of the array and the position of it
+                computerSelectionArrLines.push(answerArr[i]);
+                computerSelectionArrIndex.push(i);
             }
 
-
-
         }
-        //Function that will change the - to letters 
-        lineDisplay(computerSelection);
+
+
+        
+        console.log("computerSelectionArrIndex " + computerSelectionArrIndex)
+        console.log("computerSelection" + computerSelection)
+
+        //Append the answer in the code so iI can change its positions
+        $("#wordGuess").append(answerArr);
+
+        //Create an array that will print the number of index of of those elements
+
+
+        //Create a function that will change those
+
+
+
+
+
+
+        function scoreCal() {
+            //I compare the lenght of the word selected with the numer of points the aquire whrn they win
+            if (computerSelection.length === secretPoints) {
+                alert("YOU WON!")
+                wins++
+
+            }
+            else {
+                var guessCompleted = computerSelection.length - secretPoints
+                console.log("Guesses " + guessCompleted);
+
+            };
+        }
+
+
 
 
 
@@ -84,29 +110,48 @@ $(document).ready(function () {
 
         //Checking the letters inside the string selected
         function checkingLetter(selection) {
-            for (var i = 0; i < desingers[arrNum].length; i++) {
+            for (var i = 0; i < computerSelection.length; i++) {
                 // Found charAt in w3 school "The charAt() method returns the character at the specified index in a string.""
                 //Ask? why do we need to add string if it was a string before.
-                var letterArray = String(desingers[arrNum]).charAt(i);
+
 
                 //creating an if that runs through all of the letters of the selected index and compares it with the letter selected
-                if (selection === letterArray.toLowerCase()) {
+                if (selection === computerSelection[i].toLowerCase()) {
                     alert("RIGHT!")
 
-                    //Add the letter in the space of the line
-                    $("#wordGuessAdd").append(selection);
-                    console.log(selection);
+
+                    changeLinesToLetters(selection);
 
                     //If they have it right they dont loose guesses
-                    guessLeft++;
+                    guessLeft = guessLeft;
+
+                    //Add the letter in that specific index of 
                     $("#guessesRemain").text(guessLeft);
                     console.log(guessLeft);
+
                 }
                 else {
                     //Create variable to show guesses you have left
 
                 }
             }
+        }
+
+
+        function changeLinesToLetters(selection) {
+            //create a for loop that will go through all the letters and change them to - and the space to ""
+            for (var i = 0; i < computerSelection.length; i++) {
+
+                if (computerSelection[i] === selection) {
+                    // Add space into HTML (search the solution on google with meetup team)
+                    computerSelectionArrLines[i] = selection;
+                    $("#wordGuess").text(computerSelectionArrLines);
+                }
+    
+            }
+
+
+
         }
 
 
